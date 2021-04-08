a4ffbd31209034ec55694a7e80c483fd7db2ef18
sakuya2007 <73406242+sakuya2007@users.noreply.github.com>
2021-04-08 14:15:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,4 @@
 // コンソールにHello World!を出力する
 console.log('Hello World');
 
-var canvas = document.getElementById('sanple');
 
-var context = canvas.getContext('2d');
-
-  context.fillRect(20,40,50,100);
-  context.fillStyle = 'blue';
